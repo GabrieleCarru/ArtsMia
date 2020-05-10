2b6044adaa1e1a38317a5cae4a7e544bcd01a8f7
gabrielecarru <gabrielecarru@mbp-di-gabriele>
2020-05-10 20:12:01 +0200

    Inizio esercizio, vertici inseriti mancano archi.

--- src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
+++ src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
@@ -6,12 +6,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.artsmia.model.ArtObject;
 
 public class ArtsmiaDAO {
 
-	public List<ArtObject> listObjects() {
+	/*
+	 *  Questo metodo non fa uso dell idMap, perciò bisogna un po' modificarlo.
+	 *  Per vedere la versione originale controllare su GitHub tdp 2020 il progetto master.
+	 */
+	
+	public void listObjects(Map<Integer, ArtObject> idMap) {
 		
 		String sql = "SELECT * from objects";
 		List<ArtObject> result = new ArrayList<>();
@@ -22,19 +28,21 @@ public class ArtsmiaDAO {
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
 
-				ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
-						res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
-						res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
-						res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
+				if(!idMap.containsKey(res.getInt("object_id"))) {
 				
-				result.add(artObj);
+					ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
+							res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
+							res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
+							res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
+					
+					idMap.put(artObj.getId(), artObj);
+				}
+				
 			}
 			conn.close();
-			return result;
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null;
 		}
 	}
 	
--- src/main/java/it/polito/tdp/artsmia/model/Model.java
+++ src/main/java/it/polito/tdp/artsmia/model/Model.java
@@ -1,5 +1,51 @@
 package it.polito.tdp.artsmia.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
 public class Model {
+	
+	/*
+	 * 	Il grafo dovrà essere pesato,
+	 *  semplice e non orientato. I vertici rappresentano tutti
+	 *  gli oggetti presenti nel database (tabella objects)
+	 */
+	
+	private Graph<ArtObject, DefaultWeightedEdge> grafo;
+	
+	/*
+	 *  Perchè vogliamo usare una Map? 
+	 *  Per salvare gli oggetti che creiamo, così rimangono salvati e, 
+	 *  ogni volta che ci servono, andiamo a prenderli dalla Map senza dover fare una new.
+	 */
+	private Map<Integer, ArtObject> idMap;
+	
+	
+	public Model() {
+		idMap = new HashMap<Integer, ArtObject>();
+	}
+	
+	/*
+	 *  Utilizzo un metodo per creare il grafo anzichè crearlo all'interno di 
+	 *  'public Model()' per evitare che, qualora volessi creare un nuovo grafo, 
+	 *  mi manchi il metodo per farlo.
+	 */
+	public void creaGrafo() {
+		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		ArtsmiaDAO dao = new ArtsmiaDAO();
+		dao.listObjects(idMap);
+		
+		// Aggiungo vertici!
+		Graphs.addAllVertices(this.grafo, idMap.values());
+		
+		// Ora bisogna aggiungere gli archi! (Min 21.28 L25)
+	}
 
 }
